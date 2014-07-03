04b3a630b96ab44688be1e175cb6052b98bd1128
Oleksandr_Raskosov <Oleksandr_Raskosov@epam.com>
2014-07-03 12:43:24 +0300

    Implement UnitTest's "tearDown" method

--- cosmo_tester/test_suites/test_libcloud_ec2_core/test_libcloud_ec2_core.py
+++ cosmo_tester/test_suites/test_libcloud_ec2_core/test_libcloud_ec2_core.py
@@ -14,8 +14,8 @@ from libcloud.compute.types import NodeState
 
 
 PREFIX_RANDOM_CHARS = 3
-CLOUDIFY_TEST_CONFIG_PATH = 'd:/projects/github/cloudify-libcloud-provider/cloudify_libcloud/cloudify-config.yaml'
-CLOUDIFY_TEST_DEFAULTS_CONFIG_PATH = 'd:/projects/github/cloudify-libcloud-provider/cloudify_libcloud/cloudify-config.defaults.yaml'
+CLOUDIFY_TEST_CONFIG_PATH = 'ENTER-TEST-CONFIG-PATH-HERE'
+CLOUDIFY_TEST_DEFAULTS_CONFIG_PATH = 'ENTER-DEFAULT-TEST-CONFIG-PATH-HERE'
 
 
 class LibcloudEC2ProviderTest(unittest.TestCase):
@@ -31,14 +31,58 @@ class LibcloudEC2ProviderTest(unittest.TestCase):
         self.name_prefix = 'libcloud_ec2_test_{0}_'\
             .format(''.join(
                 random.choice(chars) for x in range(PREFIX_RANDOM_CHARS)))
-        self.provider_config = self._read_config(CLOUDIFY_TEST_CONFIG_PATH, CLOUDIFY_TEST_DEFAULTS_CONFIG_PATH)
+        self.provider_config = self._read_config(
+            CLOUDIFY_TEST_CONFIG_PATH,
+            CLOUDIFY_TEST_DEFAULTS_CONFIG_PATH)
         self._update_config(self.name_prefix, self.provider_config)
-        self.provider_manager = common.ProviderManager(provider_config=self.provider_config)
+        self.provider_manager =\
+            common.ProviderManager(provider_config=self.provider_config)
         self.driver = self.provider_manager.get_driver(self.provider_config)
         self.logger.debug("Libcloud provider for EC2 test setUp() done")
 
     def tearDown(self):
-        super(LibcloudEC2ProviderTest, self).tearDown()
+        self.logger.debug("Libcloud provider for EC2 test tearDown() called")
+        created, created_names =\
+            self._get_node_list_w_names(not_in_state=NodeState.TERMINATED)
+        if created:
+            self.logger.debug('Not all created nodes were deleted'
+                              ' during teardown process.')
+            for node in created:
+                self.driver.server_controller.kill(node)
+                self.logger.debug('Node \'{0}\' is deleted'.format(node.name))
+        else:
+            self.logger.debug('All created nodes were successfully deleted'
+                              ' during teardown process.')
+
+        created = self._get_sg_list_names()
+        if created:
+            self.logger.debug('Not all created security groups were deleted'
+                              ' during teardown process.')
+            to_delete = []
+            for sg_name in created:
+                group = self.driver.sg_controller.get_by_name(sg_name)
+                self.driver.sg_controller.remove_rules(group)
+                to_delete.append(group)
+            for group in to_delete:
+                self.driver.sg_controller.kill(group)
+                self.logger.debug('Security group \'{0}\' is deleted'
+                                  .format(group.name))
+        else:
+            self.logger.debug('All created security groups were successfully'
+                              ' deleted during teardown process.')
+
+        created, created_names = self._get_key_pair_list_w_names()
+        if created:
+            self.logger.debug('Not all created key pairs were deleted'
+                              ' during teardown process.')
+            for pair in created:
+                self.driver.keypair_controller.kill(pair)
+                self.logger.debug('Key pair \'{0}\' is deleted'
+                                  .format(pair.name))
+        else:
+            self.logger.debug('All created key pairs were successfully deleted'
+                              ' during teardown process.')
+        self.logger.debug("Libcloud provider for EC2 test tearDown() done")
 
     def test(self):
         self.logger.debug("*****Libcloud test for EC2 started*****")
@@ -97,21 +141,22 @@ class LibcloudEC2ProviderTest(unittest.TestCase):
             ' but created {0}: {1}'.format(created_len, ', '.join(created)))
 
     def _validate_key_pairs(self, compute_config):
-        created = self._get_key_pair_list_names()
+        created, created_names = self._get_key_pair_list_w_names()
         self.assertIn(
             compute_config['management_server']['management_keypair']['name'],
-            created,
+            created_names,
             'ERROR: Management key pair wasn\'t created')
         self.assertIn(
             compute_config['agent_servers']['agents_keypair']['name'],
-            created,
+            created_names,
             'ERROR: Agents key pair wasn\'t created')
         created_len = len(created)
         self.assertEqual(
             created_len,
             2,
             'ERROR: Two key pairs should be created'
-            ' but created {0}: {1}'.format(created_len, ', '.join(created)))
+            ' but created {0}: {1}'
+            .format(created_len, ', '.join(created_names)))
 
     def _validate_publick_ip(self, floating_ip_config, public_ip):
         if 'ip' in floating_ip_config:
@@ -133,7 +178,8 @@ class LibcloudEC2ProviderTest(unittest.TestCase):
             created_len,
             1,
             'ERROR: The only one management server should be created'
-            ' but created {0}: {1}'.format(created_len, ', '.join(created_names)))
+            ' but created {0}: {1}'
+            .format(created_len, ', '.join(created_names)))
         node = created[0]
         required_image = management_config['instance']['image']
         provided_image = node.extra['image_id']
@@ -152,7 +198,8 @@ class LibcloudEC2ProviderTest(unittest.TestCase):
             ' required - {0}, provided - {1}'
             .format(required_size, provided_size))
         if 'floating_ip' in management_config:
-            self._validate_publick_ip(management_config['floating_ip'], public_ip)
+            self._validate_publick_ip(management_config['floating_ip'],
+                                      public_ip)
 
     def _validate_compute(self, compute_config, public_ip):
         self._validate_key_pairs(compute_config)
@@ -166,13 +213,14 @@ class LibcloudEC2ProviderTest(unittest.TestCase):
             0,
             'ERROR: Not all created security groups were deleted'
             ' during teardown process: ' + ', '.join(created))
-        created = self._get_key_pair_list_names()
+        created, created_names = self._get_key_pair_list_w_names()
         self.assertEqual(
             len(created),
             0,
             'ERROR: Not all created key pairs were deleted'
             ' during teardown process: ' + ', '.join(created))
-        created, created_names = self._get_node_list_w_names(NodeState.TERMINATED)
+        created, created_names =\
+            self._get_node_list_w_names(not_in_state=NodeState.TERMINATED)
         self.assertEqual(
             len(created_names),
             0,
@@ -188,17 +236,19 @@ class LibcloudEC2ProviderTest(unittest.TestCase):
                 created.append(group)
         return created
 
-    def _get_key_pair_list_names(self):
+    def _get_key_pair_list_w_names(self):
         created = []
+        created_names = []
         keys = self.driver.keypair_controller.list()
         prfx = self.name_prefix
         for key in keys:
             name = key.name
             if name.startswith(prfx):
-                created.append(name)
-        return created
+                created.append(key)
+                created_names.append(name)
+        return created, created_names
 
-    def _get_node_list_w_names(self, state=None):
+    def _get_node_list_w_names(self, not_in_state=None):
         created = []
         created_names = []
         nodes = self.driver.server_controller.list()
@@ -206,8 +256,8 @@ class LibcloudEC2ProviderTest(unittest.TestCase):
         for node in nodes:
             name = node.name
             if name.startswith(prfx):
-                if state:
-                    if state == node.state:
+                if not_in_state:
+                    if not_in_state != node.state:
                         created.append(node)
                         created_names.append(name)
                 else:
