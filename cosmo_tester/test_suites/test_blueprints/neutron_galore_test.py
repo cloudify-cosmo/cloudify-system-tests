e4d70f83fe243fbddf64bcd66e7da110a0f9efa7
Idan Moyal <idan@gigaspaces.com>
2015-03-01 14:39:24 +0200

    repetitive post_install_assertions

--- cosmo_tester/test_suites/test_blueprints/neutron_galore_test.py
+++ cosmo_tester/test_suites/test_blueprints/neutron_galore_test.py
@@ -47,7 +47,7 @@ class NeutronGaloreTest(TestCase):
         node_states = self.get_delta_node_states(before, after)
 
         self.repetitive(self.post_install_assertions,
-                        timeout=120,
+                        timeout=120000,
                         args=[node_states])
 
         self._test_use_external_resource(inputs=inputs)
