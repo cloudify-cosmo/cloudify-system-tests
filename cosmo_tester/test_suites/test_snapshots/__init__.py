fe61ed5af5283fe428399d7fc279b1ad3d354de6
Idan Moyal <idan@gigaspaces.com>
2016-11-02 13:15:20 +0200

    enable snapshots tests

--- cosmo_tester/test_suites/test_snapshots/__init__.py
+++ cosmo_tester/test_suites/test_snapshots/__init__.py
@@ -14,6 +14,3 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
-
-def setUp():
-    raise RuntimeError('Snapshot tests are currently skipped.')
