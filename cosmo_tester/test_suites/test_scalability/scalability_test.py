110ab552c107a8d32b04026910a36a4e57bb3d35
Dmitry Gertsman <dmitry@example.com>
2017-02-05 12:59:44 +0200

    CFY-6168 Scalability base functionality

--- cosmo_tester/test_suites/test_scalability/scalability_test.py
+++ cosmo_tester/test_suites/test_scalability/scalability_test.py
@@ -15,70 +15,105 @@
 
 from uuid import uuid4
 from time import sleep
-from threading import Thread
+import multiprocessing
+import Queue
 
 from cosmo_tester.framework.testenv import TestCase
 from cosmo_tester.framework.util import create_rest_client
 
+MANAGER_IP = '185.98.150.115'
+MANAGER_USERNAME = 'admin'
+MANAGER_PASSWORD = 'NcM2YizuzatJ'
+NUM_OF_BLUEPRINTS = 20
+REQUESTS_INTERVAL = 2
+CREATE_DEPLOYMENT_COUNT = 2
+NUM_OF_PROC = 5
+
 
 class ScalabilityTest(TestCase):
-    MANAGER_IP = '185.98.150.115'
-    MANAGER_USERNAME = ''
-    MANAGER_PASSWORD = ''
 
     def test_scalability(self):
-        self.cfy.use(
-            self.MANAGER_IP,
-            manager_username=self.MANAGER_USERNAME,
-            manager_password=self.MANAGER_PASSWORD
+        self.cfy.profiles.use(
+            MANAGER_IP,
+            manager_username=MANAGER_USERNAME,
+            manager_password=MANAGER_PASSWORD
         )
         self.client = create_rest_client(
-            manager_ip=self.MANAGER_IP,
-            manager_username=self.MANAGER_USERNAME,
-            manager_password=self.MANAGER_PASSWORD
+            manager_ip=MANAGER_IP,
+            manager_username=MANAGER_USERNAME,
+            manager_password=MANAGER_PASSWORD
         )
 
-        self.blueprint_ids = []
-        self.deployment_ids = []
-        self.test_done = False
-
-        t1 = Thread(target=self._upload_blueprints, name='Upload')
-        t1.daemon = True
-        t1.start()
-
-        while not self.test_done:
-            self.client.blueprints.list()
-            sleep(3)
-
-        self._clean_manager()
-
-    def _upload_blueprints(self):
+        self.q_blueprint_ids = multiprocessing.Queue()
+        self.q_deployment_ids = multiprocessing.Queue()
+        self.blueprints_counter = 0
         blueprint_path = self.copy_blueprint('scalability')
-        self.blueprint_yaml = blueprint_path / 'blueprint.yaml'
         self.inputs_yaml = blueprint_path / 'inputs.yaml'
 
-        for i in range(3000):
+        self.logger.info('Sending HTTP requests in loop for all test run')
+        self.logger.info('Uploading {0} blueprints'.format(NUM_OF_BLUEPRINTS))
+        self.logger.info('Each {0} blueprints will create deployment'.format(CREATE_DEPLOYMENT_COUNT))
+
+        jobs = []
+        for i in range(NUM_OF_PROC):
+            p = multiprocessing.Process(name='upload_blueprints', target=self._upload_blueprints)
+            jobs.append(p)
+            p.start()
+
+        r = multiprocessing.Process(name='send_requests', target=self._send_requests)
+        r.start()
+        for p in jobs:
+            p.join()
+
+        r.terminate()
+
+    def _upload_blueprints(self):
+        for i in range(NUM_OF_BLUEPRINTS / NUM_OF_PROC):
+            self.blueprints_counter += 1
             blueprint_id = uuid4()
-            self.blueprint_ids.append(blueprint_id)
+            self.q_blueprint_ids.put(blueprint_id)
             self.cfy.blueprints.upload(
-                self.blueprint_yaml,
+                'cloudify-cosmo/cloudify-nodecellar-example',
+                blueprint_filename='openstack-blueprint.yaml',
                 blueprint_id=blueprint_id
             )
 
-            if i % 100 == 0:
+            if self.blueprints_counter % CREATE_DEPLOYMENT_COUNT == 0:
                 deployment_id = '{0}_dep'.format(blueprint_id)
-                self.deployment_ids.append(deployment_id)
+                self.q_deployment_ids.put(deployment_id)
                 self.create_deployment(
                     blueprint_id=blueprint_id,
                     deployment_id=deployment_id,
                     inputs=self.inputs_yaml
                 )
 
-        self.test_done = True
+        # self._clean_manager()
+
+    def _send_requests(self):
+        error_count = 0
+        while True:
+            try:
+                self.client.blueprints.list().items
+            except Exception:
+                error_count += 1
+                print Exception.message
+            sleep(REQUESTS_INTERVAL)
+            if error_count == 3:
+                raise Exception
 
     def _clean_manager(self):
-        for deployment_id in self.deployment_ids:
-            self.delete_deployment(deployment_id)
+        sleep(60)
+        self.logger.info('Cleaning up manager after test...')
+        while not self.q_deployment_ids.empty():
+            try:
+                deployment_id = self.q_deployment_ids.get()
+                self.delete_deployment(deployment_id)
+            except Queue.Empty:
+                break
 
-        for blueprint_id in self.blueprint_ids:
-            self.delete_blueprint(blueprint_id)
+        while not self.q_blueprint_ids.empty():
+            try:
+                blueprint_id = self.q_blueprint_ids.get()
+                self.delete_blueprint(blueprint_id)
+            except Queue.Empty:
+                break
