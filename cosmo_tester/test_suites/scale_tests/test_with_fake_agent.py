3d4902d8f6168848815ca27e023f5e805ebdb691
Matt Wheeler <m@funkyhat.org>
2017-08-30 14:09:37 +0100

    whoops

--- cosmo_tester/test_suites/scale_tests/test_with_fake_agent.py
+++ cosmo_tester/test_suites/scale_tests/test_with_fake_agent.py
@@ -28,8 +28,10 @@ def test_manager_agent_scaling(manager):
     manager.upload_plugin('agent/plugins/fake-load-plugin')
 
     shutil.copy2(
-        os.path.dirname(__file__),
-        "../../resources/agent/plugins/fake-load-plugin/plugin.yaml",
+        os.path.join(
+            os.path.dirname(__file__),
+            "../../resources/agent/plugins/fake-load-plugin/plugin.yaml",
+        ),
         blueprint_dir,
         )
     manager.client.blueprints.upload(
