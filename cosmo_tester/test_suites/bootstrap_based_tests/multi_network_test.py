96c253fbc367d8418987b00e0ae4ffe5843d5d26
Pavel Brodsky <mcouthon@gmail.com>
2017-09-17 16:49:28 +0300

    CFY-7226 Implement Multi Network system test In this test we bootstrap a manager with 3 (configurable) networks, and create a custom hello world on each one of them. Each custom hello world has a hello-world like blueprint that runs on a different network (by setting the agent's management_network).

--- cosmo_tester/test_suites/bootstrap_based_tests/multi_network_test.py
+++ cosmo_tester/test_suites/bootstrap_based_tests/multi_network_test.py
@@ -13,9 +13,28 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
+import yaml
 import pytest
 
 from cosmo_tester.framework.cluster import CloudifyCluster
+from cosmo_tester.framework.examples.hello_world import HelloWorldExample
+from cosmo_tester.framework.util import (
+    is_community,
+    prepare_and_get_test_tenant,
+)
+
+NETWORK_CONFIG_TEMPLATE = """DEVICE="eth{0}"
+BOOTPROTO="static"
+ONBOOT="yes"
+TYPE="Ethernet"
+USERCTL="yes"
+PEERDNS="yes"
+IPV6INIT="no"
+PERSISTENT_DHCLIENT="1"
+IPADDR="1{0}.0.0.5"
+NETMASK="255.255.255.128"
+DEFROUTE="no"
+"""
 
 
 @pytest.fixture(scope='module', params=[3])
@@ -39,10 +58,83 @@ def manager(request, cfy, ssh_key, module_tmpdir, attributes, logger):
 def _preconfigure_callback(managers):
     # The preconfigure callback populates the networks config prior to the BS
     mgr = managers[0]
-    mgr.bs_inputs = {'manager_networks': mgr._attributes.networks}
+    networks = mgr._attributes.networks
+    mgr.bs_inputs = {'manager_networks': networks}
+
+    # Configure NICs in order for networking to work properly
+    _enable_nics(mgr, networks, mgr._tmpdir, mgr._logger)
 
 
-def test_multiple_networks(manager, cfy, logger):
+def test_multiple_networks(multi_network_hello_worlds, logger):
     logger.info('Testing manager with multiple networks')
-    manager.use()
-    manager.stop_for_user_input()
+    for hello in multi_network_hello_worlds:
+        hello.verify_all()
+
+
+class MultiNetworkHelloWorld(HelloWorldExample):
+    def _patch_blueprint(self):
+        with open(self.blueprint_path, 'r') as f:
+            blueprint_dict = yaml.load(f)
+
+        node_props = blueprint_dict['node_templates']['vm']['properties']
+        agent_config = node_props['agent_config']
+        agent_config['network'] = {'get_input': 'manager_network_name'}
+
+        inputs = blueprint_dict['inputs']
+        inputs['manager_network_name'] = {}
+
+        with open(self.blueprint_path, 'w') as f:
+            yaml.dump(blueprint_dict, f)
+
+
+@pytest.fixture(scope='function')
+def multi_network_hello_worlds(cfy, manager, attributes, ssh_key, tmpdir,
+                               logger):
+    hellos = get_hello_worlds(cfy, manager, attributes, ssh_key, tmpdir,
+                              logger)
+    yield hellos
+    for hello in hellos:
+        hello.cleanup()
+
+
+def get_hello_worlds(cfy, manager, attributes, ssh_key, tmpdir, logger):
+    hellos = []
+    for network_name in attributes.networks:
+        if is_community():
+            tenant = 'default_tenant'
+        else:
+            tenant = '{0}_tenant'.format(network_name)
+            prepare_and_get_test_tenant(
+                '{0}_tenant'.format(network_name), manager, cfy
+            )
+        hello = MultiNetworkHelloWorld(
+            cfy, manager, attributes, ssh_key, logger, tmpdir,
+            tenant=tenant, suffix=tenant)
+        hello.blueprint_file = 'openstack-blueprint.yaml'
+        hello.inputs.update({
+            'agent_user': attributes.centos_7_username,
+            'image': attributes.centos_7_image_name,
+            'manager_network_name': network_name,
+            'network_name': attributes.network_names[network_name]
+        })
+        hellos.append(hello)
+    return hellos
+
+
+def _enable_nics(manager, networks, tmpdir, logger):
+    """ Extra network interfaces need to be manually enabled on the manager """
+
+    logger.info('Adding extra NICs...')
+    # Need to do this for each network except 0 (as eth0 is already enabled)
+    for i in range(1, len(networks)):
+        network_file_path = tmpdir / 'network_cfg_{0}'.format(i)
+        # Create and copy the interface config
+        network_file_path.write_text(NETWORK_CONFIG_TEMPLATE.format(i))
+        with manager.ssh() as fabric_ssh:
+            fabric_ssh.put(
+                network_file_path,
+                '/etc/sysconfig/network-scripts/ifcfg-eth{0}'.format(i),
+                use_sudo=True
+            )
+            # Start the interface
+            fabric_ssh.sudo('ifup eth{0}'.format(i))
