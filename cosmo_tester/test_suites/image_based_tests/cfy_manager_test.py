65657e68eadcd813e4d3b50808d36e1d7e2b5f70
Pavel Brodsky <mcouthon@gmail.com>
2018-10-17 15:39:35 +0300

    CY-698 Validate that RMQ passwords weren't changed by configure This is a more robust test, as it both verifies that all the users were recreated, and that the passwords and the security config have remained unchanged

--- cosmo_tester/test_suites/image_based_tests/cfy_manager_test.py
+++ cosmo_tester/test_suites/image_based_tests/cfy_manager_test.py
@@ -23,24 +23,49 @@ manager = image_based_manager
 REMOTE_CERT_PATH = '/etc/cloudify/ssl/cloudify_internal_ca_cert.pem'
 REMOTE_CONF_PATH = '/opt/manager/rest-security.conf'
 REMOTE_HOOKS_PATH = '/opt/mgmtworker/config/hooks.conf'
-LIST_MQ_USERS_COMMAND = 'rabbitmqctl -n cloudify-manager@localhost list_users'
+AUTH_MQ_USER_CMD = 'sudo rabbitmqctl -n cloudify-manager@localhost ' \
+                   'authenticate_user {user} {password}'
 
 NEW_TENANT = 'new_tenant'
 NEW_KEY = 'new_key'
 NEW_VALUE = 'new_value'
 NEW_HOOKS = 'new_hooks'
 
+GET_MQ_PASSWORDS_CODE_PATH = '/tmp/get_passwords.py'
+MQ_PASSWORDS_PATH = '/tmp/passwords'
+GET_MQ_PASSWORDS_CODE = '''
+import json
+
+from manager_rest.cryptography_utils import decrypt
+from manager_rest.storage import models
+from manager_rest.flask_utils import setup_flask_app
+
+setup_flask_app()
+
+tenants = models.Tenant.query.all()
+decrypted_passwords = {t.rabbitmq_username: 
+                           decrypt(t.rabbitmq_password) for t in tenants}
+with open('%s', 'w') as f:
+    json.dump(decrypted_passwords, f)
+''' % MQ_PASSWORDS_PATH
+
 
 def test_cfy_manager_configure(manager, logger, tmpdir):
     manager.sync_local_code_to_manager()
+
+    logger.info('Putting code to get decrypted passwords on manager...')
+    manager.put_remote_file_content(
+        remote_path=GET_MQ_PASSWORDS_CODE_PATH,
+        content=GET_MQ_PASSWORDS_CODE
+    )
 
     logger.info('Getting current CA cert from the manager...')
     old_cert = manager.get_remote_file_content(REMOTE_CERT_PATH)
 
     logger.info('Creating new tenant and validating RMQ user was created...')
     manager.client.tenants.create(NEW_TENANT)
-    output = manager.run_command(LIST_MQ_USERS_COMMAND, use_sudo=True)
-    assert NEW_TENANT in output
+    mq_passwords = _get_mq_passwords(manager)
+    assert 'rabbitmq_user_{0}'.format(NEW_TENANT) in mq_passwords
 
     logger.info('Editing security config file on the manager...')
     _edit_security_config(manager)
@@ -55,10 +80,6 @@ def test_cfy_manager_configure(manager, logger, tmpdir):
     new_cert = manager.get_remote_file_content(REMOTE_CERT_PATH)
     assert old_cert == new_cert
 
-    logger.info('Verifying RabbitMQ users recreated after configure...')
-    output = manager.run_command(LIST_MQ_USERS_COMMAND, use_sudo=True)
-    assert NEW_TENANT in output
-
     logger.info('Validating security config file on the manager persists...')
     security_config = json.loads(
         manager.get_remote_file_content(REMOTE_CONF_PATH)
@@ -72,6 +93,15 @@ def test_cfy_manager_configure(manager, logger, tmpdir):
 
     assert hooks_content == NEW_HOOKS
 
+    logger.info('Validating MQ passwords unchanged after configure...')
+    # We expect the command to fail if the password has changed or
+    # if the any of the users weren't recreated in RMQ
+    for mq_user, mq_password in mq_passwords.items():
+        manager.run_command(
+            AUTH_MQ_USER_CMD.format(user=mq_user, password=mq_password),
+            use_sudo=True
+        )
+
 
 def _edit_security_config(manager):
     security_config = json.loads(
@@ -83,3 +113,11 @@ def _edit_security_config(manager):
     manager.put_remote_file_content(
         REMOTE_CONF_PATH, json.dumps(security_config)
     )
+
+
+def _get_mq_passwords(manager):
+    manager.run_command(
+        '/opt/manager/env/bin/python {0}'.format(GET_MQ_PASSWORDS_CODE_PATH)
+    )
+    mq_passwords = manager.get_remote_file_content(MQ_PASSWORDS_PATH)
+    return json.loads(mq_passwords)
