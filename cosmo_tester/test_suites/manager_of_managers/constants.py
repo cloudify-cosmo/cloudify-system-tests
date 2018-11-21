bb64ed8c2145446705b180d67b8f99de1a1bbd6b
Pavel Brodsky <mcouthon@gmail.com>
2018-11-21 17:34:22 +0200

    CY-712 Refactor plugins related constants

--- cosmo_tester/test_suites/manager_of_managers/constants.py
+++ cosmo_tester/test_suites/manager_of_managers/constants.py
@@ -17,15 +17,23 @@ MOM_PLUGIN_VERSION = '1.5.6'
 MOM_PLUGIN_WGN_URL = 'https://github.com/Cloudify-PS/manager-of-managers/releases/download/v{0}/cloudify_manager_of_managers-{0}-py27-none-linux_x86_64.wgn'.format(MOM_PLUGIN_VERSION)  # NOQA
 MOM_PLUGIN_YAML_URL = 'https://github.com/Cloudify-PS/manager-of-managers/releases/download/v{0}/cmom_plugin.yaml'.format(MOM_PLUGIN_VERSION)  # NOQA
 
+OS_WGN_FILENAME_TEMPLATE = 'cloudify_openstack_plugin-{0}-py27-none-linux_x86_64-centos-Core.wgn'  # NOQA
+OS_YAML_URL_TEMPLATE = 'http://www.getcloudify.org/spec/openstack-plugin/{0}/plugin.yaml'  # NOQA
+OS_WGN_URL_TEMPLATE = 'http://repository.cloudifysource.org/cloudify/wagons/cloudify-openstack-plugin/{0}/{1}'  # NOQA
+
 # This version of the plugin is used by the mom blueprint
 OS_PLUGIN_VERSION = '2.12.0'
-OS_PLUGIN_WGN_FILENAME = 'cloudify_openstack_plugin-{0}-py27-none-linux_x86_64-centos-Core.wgn'.format(OS_PLUGIN_VERSION)  # NOQA
-OS_PLUGIN_WGN_URL = 'http://repository.cloudifysource.org/cloudify/wagons/cloudify-openstack-plugin/{0}/{1}'.format(OS_PLUGIN_VERSION, OS_PLUGIN_WGN_FILENAME)  # NOQA
-OS_PLUGIN_YAML_URL = 'http://www.getcloudify.org/spec/openstack-plugin/{0}/plugin.yaml'.format(OS_PLUGIN_VERSION)  # NOQA
+OS_PLUGIN_WGN_FILENAME = OS_WGN_FILENAME_TEMPLATE.format(OS_PLUGIN_VERSION)
+OS_PLUGIN_WGN_URL = OS_WGN_URL_TEMPLATE.format(OS_PLUGIN_VERSION,
+                                               OS_PLUGIN_WGN_FILENAME)
+OS_PLUGIN_YAML_URL = OS_YAML_URL_TEMPLATE.format(OS_PLUGIN_VERSION)
 
-# Using 2.0.1 because this is what the hello world blueprint is using
-OS_201_PLUGIN_WGN_URL = 'http://repository.cloudifysource.org/cloudify/wagons/cloudify-openstack-plugin/2.0.1/cloudify_openstack_plugin-2.0.1-py27-none-linux_x86_64-centos-Core.wgn'  # NOQA
-OS_201_PLUGIN_YAML_URL = 'http://www.getcloudify.org/spec/openstack-plugin/2.0.1/plugin.yaml'  # NOQA
+# The version of the OS plugin used by Hello World Example
+HW_OS_PLUGIN_VERSION = '2.0.1'
+HW_OS_WGN_FILENAME = OS_WGN_FILENAME_TEMPLATE.format(HW_OS_PLUGIN_VERSION)
+HW_OS_PLUGIN_WGN_URL = OS_WGN_URL_TEMPLATE.format(HW_OS_PLUGIN_VERSION,
+                                                  HW_OS_WGN_FILENAME)
+HW_OS_PLUGIN_YAML_URL = OS_YAML_URL_TEMPLATE.format(HW_OS_PLUGIN_VERSION)
 
 HELLO_WORLD_URL = 'https://github.com/cloudify-cosmo/cloudify-hello-world-example/archive/4.5.zip'  # NOQA
 
@@ -38,8 +46,8 @@ SECOND_DEP_INDICATOR = '1'
 TIER_2_SNAP_ID = 'snapshot_id'
 
 INSTALL_RPM_PATH = '/etc/cloudify/cloudify-manager-install.rpm'
-PLUGIN_WGN_PATH = '/etc/cloudify/{0}'.format(OS_PLUGIN_WGN_FILENAME)
-PLUGIN_YAML_PATH = '/etc/cloudify/plugin.yaml'
+HW_OS_PLUGIN_WGN_PATH = '/etc/cloudify/{0}'.format(HW_OS_WGN_FILENAME)
+HW_OS_PLUGIN_YAML_PATH = '/etc/cloudify/plugin.yaml'
 
 SECRET_STRING_KEY = 'test_secret_from_string'
 SECRET_STRING_VALUE = 'test_secret_value'
