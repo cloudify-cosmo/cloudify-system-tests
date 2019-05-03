5de240fff13b556c2298984a25a5aa92e4ba51e0
asdf <tehasdf@gmail.com>
2019-05-03 15:03:08 +0200

    Delete blueprints that are not used in any test

--- cosmo_tester/resources/blueprints/openstack-cinder/scripts/write-to-volume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-# injected by blueprint
-mount_point=${mount_point}
-
-ctx logger info "Attempting to write a file in ${mount_point}"
-echo "touched" > ${mount_point}/test.txt
-ctx logger info "Successfully wrote a file in ${mount_point}"
