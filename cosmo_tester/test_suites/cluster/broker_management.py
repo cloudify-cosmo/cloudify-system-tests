0a3af73a58c62697eef79b92ea8b6f2562622c6c
Madda <nobody@nowhere.local>
2019-10-18 14:24:37 +0000

    Move all broker management tests to one file

--- cosmo_tester/test_suites/cluster/broker_management.py
+++ cosmo_tester/test_suites/cluster/broker_management.py
@@ -1,3 +1,4 @@
+import json
 import time
 
 
@@ -17,6 +18,14 @@ def get_broker_listing(broker, prefix='rabbit@'):
             'alarms': broker_alarms,
         }
     return brokers
+
+
+def manager_list_brokers(manager):
+    return json.loads(
+        manager.run_command(
+            'cfy cluster brokers list --json 2>/dev/null | cat'
+        )
+    )
 
 
 def add_to_hosts(target_broker, new_entry_brokers):
@@ -288,3 +297,53 @@ def test_remove(brokers, logger):
         )
     get_cluster_listing([brokers[0]])
     logger.info('Cluster failure recovery successful.')
+
+
+def test_broker_management(brokers_and_manager, logger):
+    # All in one test for speed until such time as complexity of these
+    # operations increases to the point that extra tests are needed.
+    broker1, broker2, manager = brokers_and_manager
+
+    manager.enter_sanity_mode()
+
+    expected_1 = {
+        'port': 5671,
+        'networks': {'default': str(broker1.private_ip_address)},
+        'name': broker1.hostname,
+    }
+    broker_2_nets = {'default': str(broker2.private_ip_address),
+                     'testnet': '192.0.2.4'}
+    expected_2 = {
+        'port': 5671,
+        'networks': broker_2_nets,
+        'name': broker2.hostname,
+    }
+
+    logger.info('Confirming list functionality.')
+    brokers_list = manager_list_brokers(manager)
+    assert brokers_list == [expected_1]
+    logger.info('Listing check passed.')
+
+    logger.info('Confirming add functionality.')
+    manager.run_command(
+        'cfy cluster brokers add {name} {ip} -n "{net}"'.format(
+            name=broker2.hostname,
+            ip=str(broker2.private_ip_address),
+            net=json.dumps(broker_2_nets),
+        )
+    )
+    brokers_list = manager_list_brokers(manager)
+    assert len(brokers_list) == 2
+    assert expected_1 in brokers_list
+    assert expected_2 in brokers_list
+    logger.info('Adding broker succeeded.')
+
+    logger.info('Confirming removal functionality.')
+    manager.run_command(
+        'cfy cluster brokers remove {name}'.format(
+            name=broker1.hostname,
+        )
+    )
+    brokers_list = manager_list_brokers(manager)
+    assert brokers_list == [expected_2]
+    logger.info('Removing broker succeeded.')
