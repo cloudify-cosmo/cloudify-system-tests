4c609e1efbfbf4d420a60f08cb15fb2b24e74629
Madda <nobody@nowhere.local>
2020-04-21 08:08:12 +0000

    CY-2393 Make resume workflow test use new example

--- cosmo_tester/test_suites/cluster/full_cluster_test.py
+++ cosmo_tester/test_suites/cluster/full_cluster_test.py
@@ -189,36 +189,29 @@ def _wait_for_cfy_node_to_start_serving(cfy, timeout=15):
     raise TimeoutException
 
 
-def test_workflow_resume_manager_failover(minimal_cluster, cfy):
+def test_workflow_resume_manager_failover(minimal_cluster, cfy,
+                                          logger, ssh_key):
     broker, db, mgr1, mgr2 = minimal_cluster
     mgr1.use(cert_path=mgr1.local_ca)
-    plugin_name = 'waiting'
-    plugin_yaml = '{}-blueprint.yaml'.format(plugin_name)
-    plugin_wagon = 'testplugin-0.0.0-py27-none-any.wgn'
 
-    # upload a mock plugin which waits 1 minute, deploy and execute
-    tests_root = path.dirname(path.dirname(path.dirname(
-        path.realpath(__file__))))
-    blueprints_dir = path.join(tests_root, 'resources', 'blueprints', 'mocks')
-    cfy.blueprints.upload(path.join(blueprints_dir, plugin_yaml),
-                          '-b', plugin_name)
-    cfy.plugins.upload(path.join(blueprints_dir, plugin_wagon), '-y',
-                       path.join(blueprints_dir, plugin_yaml))
-    cfy.deployments.create(plugin_name, '-b', plugin_name)
-    _wait_for_deployment_creation(cfy, plugin_name)
+    example = get_example_deployment(cfy, mgr1, ssh_key, logger,
+                                     'workflow_resume_manager_failover',
+                                     using_agent=False)
+    example.inputs['wait'] = 60
+    example.upload_blueprint()
+    example.create_deployment()
+    _wait_for_deployment_creation(cfy, example)
     execution_start_time = time.time()
     with set_client_tenant(mgr1, example.tenant):
-        mgr1.client.executions.start(plugin_name, 'install')
-
-        # check which management worker handles the execution
-        exec_node_id = mgr1.client.node_instances.list()[0].id
+        exec_id = mgr1.client.executions.start(example.deployment_id,
+                                               'install').id
     time.sleep(3)   # wait for mgmtworker to get the execution
     executing_manager = None
     other_manager = None
     for manager in [mgr1, mgr2]:
         try:
             manager.run_command('grep {} /var/log/cloudify/mgmtworker/'
-                                'mgmtworker.log'.format(exec_node_id))
+                                'mgmtworker.log'.format(exec_id))
         except Exception:
             continue
         executing_manager = manager
@@ -233,17 +226,23 @@ def test_workflow_resume_manager_failover(minimal_cluster, cfy):
     other_manager.use(cert_path=other_manager.local_ca)
 
     # verify on the second manager that the execution completed successfully
-    executions = json.loads(cfy.executions.list('--json').stdout)
+    executions = json.loads(cfy.executions.list(
+        '--tenant-name', example.tenant,
+        '--json').stdout)
     installs = [execution for execution in executions
                 if execution['workflow_id'] == 'install']
     assert len(installs) == 1
     assert installs[0]['status'] == 'completed'
 
 
-def _wait_for_deployment_creation(cfy, deployment_id, timeout=15):
+def _wait_for_deployment_creation(cfy, example, timeout=30):
     for _ in range(timeout):
         time.sleep(1)
-        args = ['--json', '-d', deployment_id]
+        args = [
+            '--json',
+            '--deployment-id', example.deployment_id,
+            '--tenant-name', example.tenant,
+        ]
         deployment_create_execs = json.loads(cfy.executions.list(args).stdout)
         if (len(deployment_create_execs) > 0 and
                 deployment_create_execs[0]['status'] == 'completed'):
