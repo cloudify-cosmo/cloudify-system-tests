1ee0f4a1c14d6acf08466cf2b3c56b41230a8fdd
Madda <nobody@nowhere.local>
2020-05-01 14:10:45 +0000

    CY-2393 Update linux CLI tests for new examples

--- cosmo_tester/test_suites/cli/linux_test.py
+++ cosmo_tester/test_suites/cli/linux_test.py
@@ -13,6 +13,8 @@
 #    * See the License for the specific language governing permissions and
 #    * limitations under the License.
 
+import json
+import os
 
 import pytest
 
@@ -23,9 +25,9 @@ from cosmo_tester.framework.util import (
 )
 from cosmo_tester.framework.test_hosts import (
     get_image,
-    REMOTE_PRIVATE_KEY_PATH,
-    TestHosts,
+    TestHosts as Hosts,
 )
+from cosmo_tester.framework.examples import get_example_deployment
 
 
 def get_linux_image_settings():
@@ -53,7 +55,7 @@ def linux_cli_tester(request, cfy, ssh_key, module_tmpdir, attributes,
     instances[0].image_name = request.param[0]
     instances[0].username = request.param[1]
 
-    cli_hosts = TestHosts(
+    cli_hosts = Hosts(
         cfy, ssh_key, module_tmpdir,
         attributes, logger, instances=instances, request=request,
         upload_plugins=False,
@@ -68,27 +70,57 @@ def linux_cli_tester(request, cfy, ssh_key, module_tmpdir, attributes,
     cli_hosts.destroy()
 
 
-def test_cli_on_linux(linux_cli_tester, attributes):
+def test_cli_on_linux(linux_cli_tester, module_tmpdir, ssh_key, logger):
     cli_host, manager_host = linux_cli_tester['cli_hosts'].instances
 
-    local_script_path = get_resource_path(
-        'scripts/linux-cli-test'
+    local_install_script_path = get_resource_path(
+        'scripts/linux-cli-test-install'
     )
-    remote_script_path = '/tmp/linux-cli-test'
-
+    remote_install_script_path = '/tmp/linux-cli-test-install'
     cli_host.put_remote_file(
-        remote_path=remote_script_path,
-        local_path=local_script_path,
+        remote_path=remote_install_script_path,
+        local_path=local_install_script_path,
     )
-    cli_host.run_command('chmod 500 {}'.format(remote_script_path))
+    cli_host.run_command('chmod 500 {}'.format(remote_install_script_path))
+
+    local_uninstall_script_path = get_resource_path(
+        'scripts/linux-cli-test-uninstall'
+    )
+    remote_uninstall_script_path = '/tmp/linux-cli-test-uninstall'
+    cli_host.put_remote_file(
+        remote_path=remote_uninstall_script_path,
+        local_path=local_uninstall_script_path,
+    )
+    cli_host.run_command('chmod 500 {}'.format(remote_uninstall_script_path))
+
+    example = get_example_deployment(
+        manager_host, ssh_key, logger, linux_cli_tester['url_key'])
+    example.set_agent_key_secret()
+    example.inputs['path'] = '/tmp/{}'.format(linux_cli_tester['url_key'])
+    cli_host.run_command('mkdir -p /tmp/test_blueprint')
+    cli_host.put_remote_file(
+        remote_path='/tmp/test_blueprint/blueprint.yaml',
+        local_path=example.blueprint_file,
+    )
+    local_inputs_path = os.path.join(
+        module_tmpdir, linux_cli_tester['url_key'] + '_inputs.yaml',
+    )
+    with open(local_inputs_path, 'w') as inputs_handle:
+        inputs_handle.write(json.dumps(example.inputs))
+    cli_host.put_remote_file(
+        remote_path='/tmp/test_blueprint/inputs.yaml',
+        local_path=local_inputs_path,
+    )
 
     cli_host.run_command(
-        '{script} {cli_url} {key} {mgr_pub} {mgr_priv} {mgr_os_user}'.format(
-            script=remote_script_path,
+        '{script} {cli_url} {mgr_priv} {tenant}'.format(
+            script=remote_install_script_path,
             cli_url=get_cli_package_url(linux_cli_tester['url_key']),
-            key=REMOTE_PRIVATE_KEY_PATH,
-            mgr_pub=manager_host.ip_address,
             mgr_priv=manager_host.private_ip_address,
-            mgr_os_user=linux_cli_tester['username'],
+            tenant=example.tenant,
         )
     )
+    example.check_files()
+
+    cli_host.run_command(remote_uninstall_script_path)
+    example.check_all_test_files_deleted()
