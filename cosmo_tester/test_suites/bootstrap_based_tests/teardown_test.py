ae0b716438256cac77236d1fe71fdc9029a323ed
Madda <nobody@nowhere.local>
2020-06-02 15:42:35 +0000

    Always block on bootstrap for teardown test

--- cosmo_tester/test_suites/bootstrap_based_tests/teardown_test.py
+++ cosmo_tester/test_suites/bootstrap_based_tests/teardown_test.py
@@ -20,7 +20,7 @@ pre_bootstrap_state = None
 
 def test_teardown(bootstrap_test_manager, ssh_key, logger, test_config):
     check_pre_bootstrap_state(bootstrap_test_manager)
-    bootstrap_test_manager.bootstrap()
+    bootstrap_test_manager.bootstrap(blocking=True)
 
     bootstrapped_state = _get_system_state(bootstrap_test_manager)
     expected_diffs = {}
