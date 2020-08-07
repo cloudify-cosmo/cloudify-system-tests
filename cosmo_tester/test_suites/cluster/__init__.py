c50bcebba1a6f67158e0ee2394659696538a2e57
Madda <nobody@nowhere.local>
2020-08-07 14:24:04 +0000

    CY-3253 Don't remove deployment in cluster test
    It's needed to test that the install still works afterwards.

--- cosmo_tester/test_suites/cluster/__init__.py
+++ cosmo_tester/test_suites/cluster/__init__.py
@@ -1,7 +1,7 @@
 def check_managers(mgr1, mgr2, example):
     # Check each manager can run workflows independently
     mgr2.run_command('sudo systemctl stop cloudify-mgmtworker')
-    example.uninstall()
+    example.uninstall(delete_dep=False)
     mgr2.run_command('sudo systemctl start cloudify-mgmtworker')
     mgr1.run_command('sudo systemctl stop cloudify-mgmtworker')
     example.install()
