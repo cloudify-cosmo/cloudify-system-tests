1ba4a58482fa5813e01d41f25c4550a3ae080435
Jakub Madej <jakub.madej@amartus.com>
2020-10-02 15:43:23 +0200

    Use coverage instrumented stage build unconditionally

--- cosmo_tester/test_suites/ui_tests/stage_test.py
+++ cosmo_tester/test_suites/ui_tests/stage_test.py
@@ -6,20 +6,20 @@ def test_stage(test_ui_manager, ssh_key, logger, test_config):
     logger.info('Installing dependencies to run system tests...')
     subprocess.call(['npm', 'run', 'beforebuild'],
                     cwd=test_config['ui']['stage_repo'])
-    if test_config['ui']['update']:
-        logger.info('Starting update of Stage package on Manager...')
-        logger.info('Creating Stage package...')
-        subprocess.call(['npm', 'run', 'build'],
-                        cwd=test_config['ui']['stage_repo'])
-        subprocess.call(['npm', 'run', 'zip'],
-                        cwd=test_config['ui']['stage_repo'])
 
-        logger.info('Uploading Stage package...')
-        os.environ["MANAGER_USER"] = test_ui_manager.username
-        os.environ["MANAGER_IP"] = test_ui_manager.ip_address
-        os.environ["SSH_KEY_PATH"] = ssh_key.private_key_path
-        subprocess.call(['npm', 'run', 'upload'],
-                        cwd=test_config['ui']['stage_repo'])
+    logger.info('Starting update of Stage package on Manager...')
+    logger.info('Creating Stage package...')
+    subprocess.call(['npm', 'run', 'build:coverage'],
+                    cwd=test_config['ui']['stage_repo'])
+    subprocess.call(['npm', 'run', 'zip'],
+                    cwd=test_config['ui']['stage_repo'])
+
+    logger.info('Uploading Stage package...')
+    os.environ["MANAGER_USER"] = test_ui_manager.username
+    os.environ["MANAGER_IP"] = test_ui_manager.ip_address
+    os.environ["SSH_KEY_PATH"] = ssh_key.private_key_path
+    subprocess.call(['npm', 'run', 'upload'],
+                    cwd=test_config['ui']['stage_repo'])
 
     logger.info('Starting Stage system tests...')
     os.environ["STAGE_E2E_MANAGER_URL"] = test_ui_manager.ip_address
