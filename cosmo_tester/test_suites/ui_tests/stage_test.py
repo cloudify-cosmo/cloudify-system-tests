8336970ffc0dbc3e6e1cec90f88606a5a991e476
Jakub Madej <jakub.madej@amartus.com>
2020-12-15 13:48:09 +0100

    Build fix

--- cosmo_tester/test_suites/ui_tests/stage_test.py
+++ cosmo_tester/test_suites/ui_tests/stage_test.py
@@ -7,4 +7,4 @@ def test_stage(test_ui_manager, ssh_key, logger, test_config):
     os.environ["MANAGER_IP"] = test_ui_manager.ip_address
     os.environ["SSH_KEY_PATH"] = ssh_key.private_key_path
     subprocess.check_call(['npm', 'run', 'e2e:ci'],
-                            cwd=test_config['ui']['stage_repo'])
+                        cwd=test_config['ui']['stage_repo'])
