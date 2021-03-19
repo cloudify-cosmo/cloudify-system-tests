8d5c7509aea86bcb4035753149a178d41b756933
Madda <nobody@nowhere.local>
2021-03-19 12:32:40 +0000

    RD-1677 Test deployment update after snapshot

--- cosmo_tester/test_suites/image_based_tests/deployment_update_test.py
+++ cosmo_tester/test_suites/image_based_tests/deployment_update_test.py
@@ -1,16 +1,9 @@
 import pytest
 
-from retrying import retry
-
-from cosmo_tester.framework import util
-from cosmo_tester.framework.examples import get_example_deployment
-from cosmo_tester.framework.util import (
-    set_client_tenant,
-    wait_for_blueprint_upload
+from cosmo_tester.framework.deployment_update import (
+    apply_and_check_deployment_update,
 )
-
-
-update_counter = 0
+from cosmo_tester.framework.examples import get_example_deployment
 
 
 @pytest.fixture(scope='function')
@@ -27,104 +20,10 @@ def test_simple_deployment_update(image_based_manager,
                                   logger):
     example_deployment.upload_and_verify_install()
 
-    modified_blueprint_path = util.get_resource_path(
-        'blueprints/compute/example_2_files.yaml'
-    )
-    blueprint_id = 'updated'
-    with set_client_tenant(image_based_manager.client,
-                           example_deployment.tenant):
-        image_based_manager.client.blueprints.upload(
-            modified_blueprint_path,
-            blueprint_id,
-        )
-        wait_for_blueprint_upload(image_based_manager.client, blueprint_id)
-
-    logger.info('Updating example deployment...')
-    _update_deployment(image_based_manager.client,
-                       example_deployment.deployment_id,
-                       example_deployment.tenant,
-                       blueprint_id,
-                       logger,
-                       skip_reinstall=True)
-
-    logger.info('Checking old files still exist')
-    example_deployment.check_files()
-
-    original_path = example_deployment.inputs['path']
-    updated_path = '/tmp/new_test'
-    updated_content = 'Where are the elephants?'
-
-    logger.info('Checking new files exist')
-    example_deployment.check_files(path='/tmp/test_announcement',
-                                   expected_content='I like cake')
-
-    logger.info('Preparing for updated deployment')
-    # We have to clean up beforehand because the update uses the new inputs
-    # instead of the old ones for the uninstall, which will fail if we don't
-    # prepare.
-    with set_client_tenant(image_based_manager.client,
-                           example_deployment.tenant):
-        inst_id = util.get_node_instances(
-            'file', example_deployment.deployment_id,
-            image_based_manager.client,
-        )[0]['id']
-    suffix = inst_id.split('_')[-1]
-    image_based_manager.run_command('rm {}_*'.format(original_path))
-    image_based_manager.put_remote_file_content(
-        remote_path='{}_file_{}'.format(updated_path, suffix),
-        content=updated_content,
-    )
-
-    logger.info('Updating deployment to use different path and content')
-    _update_deployment(image_based_manager.client,
-                       example_deployment.deployment_id,
-                       example_deployment.tenant,
-                       example_deployment.blueprint_id,
-                       logger,
-                       inputs={'path': updated_path,
-                               'content': updated_content})
-
-    logger.info('Checking new files were created')
-    example_deployment.check_files(
-        path=updated_path,
-        expected_content=updated_content,
-    )
-    logger.info('Checking old files were removed')
-    # This will look for the originally named files
-    example_deployment.check_all_test_files_deleted()
+    apply_and_check_deployment_update(image_based_manager, example_deployment,
+                                      logger)
 
     logger.info('Uninstalling deployment')
     example_deployment.uninstall()
     logger.info('Checking new files were removed')
-    example_deployment.check_all_test_files_deleted(path=updated_path)
-
-
-@retry(stop_max_attempt_number=10, wait_fixed=5000)
-def wait_for_deployment_update(client, execution_id, logger):
-    logger.info('Checking deployment update with execution ID: {}'.format(
-        execution_id))
-    dep_update = client.deployment_updates.list(
-        execution_id=execution_id)[0]
-    assert dep_update['state'] == 'successful'
-
-
-def _update_deployment(client,
-                       deployment_id,
-                       tenant,
-                       blueprint_id,
-                       logger,
-                       skip_reinstall=False,
-                       inputs=None):
-    with set_client_tenant(client, tenant):
-        dep_update = client.deployment_updates.update_with_existing_blueprint(
-            deployment_id=deployment_id,
-            blueprint_id=blueprint_id,
-            skip_reinstall=skip_reinstall,
-            inputs=inputs,
-        )
-        logger.info('Waiting for deployment update to complete...')
-        execution = client.executions.list(id=dep_update['execution_id'])[0]
-        util.wait_for_execution(client, execution, logger)
-
-        wait_for_deployment_update(client, dep_update['execution_id'], logger)
-        logger.info('Deployment update complete.')
+    example_deployment.check_all_test_files_deleted()
