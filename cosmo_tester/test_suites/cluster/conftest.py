cfd3d5e3dd0e9d1998c974ea096b1ade076ba0f8
Madda <nobody@nowhere.local>
2021-04-29 12:45:15 +0100

    Remove disused cluster upgrade fixtures

--- cosmo_tester/test_suites/cluster/conftest.py
+++ cosmo_tester/test_suites/cluster/conftest.py
@@ -144,26 +144,6 @@ def nine_vms(ssh_key, module_tmpdir, test_config, logger, request):
         yield _vms
 
 
-@pytest.fixture(params=['5_1_0_installer', '5_1_1_installer'])
-def three_nodes_base_cluster(ssh_key, module_tmpdir, test_config, logger,
-                             request):
-    for _vms in _get_hosts(ssh_key, module_tmpdir, test_config, logger,
-                           request, pre_cluster_rabbit=True,
-                           three_nodes_cluster=True,
-                           installer_image_name=request.param):
-        yield _vms
-
-
-@pytest.fixture(params=['5_1_0_installer', '5_1_1_installer'])
-def nine_nodes_base_cluster(ssh_key, module_tmpdir, test_config, logger,
-                            request):
-    for _vms in _get_hosts(ssh_key, module_tmpdir, test_config, logger,
-                           request, pre_cluster_rabbit=True,
-                           broker_count=3, db_count=3, manager_count=3,
-                           installer_image_name=request.param):
-        yield _vms
-
-
 def _get_hosts(ssh_key, module_tmpdir, test_config, logger, request,
                broker_count=0, manager_count=0, db_count=0,
                use_load_balancer=False, skip_bootstrap_list=None,
