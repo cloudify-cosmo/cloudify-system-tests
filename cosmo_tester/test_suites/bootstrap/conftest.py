3c81748b7458e498db272d967365be68dd90da6a
Madda <nobody@nowhere.local>
2021-06-09 16:17:14 +0100

    RD-2585 Only deploy 1 VM for bootstrap tests

--- cosmo_tester/test_suites/bootstrap/conftest.py
+++ cosmo_tester/test_suites/bootstrap/conftest.py
@@ -1,22 +1,13 @@
 import pytest
 
-from cosmo_tester.framework.test_hosts import Hosts
-
 
 @pytest.fixture(scope='function')
-def bootstrap_test_manager(request, ssh_key, module_tmpdir, test_config,
-                           logger):
+def bootstrap_test_manager(session_manager):
     """Prepares a bootstrappable manager."""
-    hosts = Hosts(
-        ssh_key, module_tmpdir, test_config, logger, request,
-        bootstrappable=True,
-    )
-    try:
-        hosts.create()
-        hosts.instances[0].wait_for_ssh()
-        # We don't bootstrap here because bootstrapping in the test means that
-        # --pdb will actually be useful as it'll allow investigation before
-        # teardown on bootstrap failure
-        yield hosts.instances[0]
-    finally:
-        hosts.destroy()
+    session_manager.wait_for_ssh()
+    # We don't bootstrap here because bootstrapping in the test means that
+    # --pdb will actually be useful as it'll allow investigation before
+    # teardown on bootstrap failure
+    yield session_manager
+    if session_manager.bootstrapped:
+        session_manager.teardown()
