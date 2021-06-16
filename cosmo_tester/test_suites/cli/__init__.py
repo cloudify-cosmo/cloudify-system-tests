6530800f2f4257f770af7d328978144f66e4b6d1
Madda <nobody@nowhere.local>
2021-06-16 16:24:18 +0100

    RD-2623 Reduce CLI VM deploy count

--- cosmo_tester/test_suites/cli/__init__.py
+++ cosmo_tester/test_suites/cli/__init__.py
@@ -3,10 +3,22 @@ import time
 import hashlib
 import tarfile
 
+from cosmo_tester.framework.examples import get_example_deployment
 from cosmo_tester.framework.util import get_cli_package_url
 
 
-def _prepare(cli_host, example, paths, logger):
+LINUX_OSES = [
+    'centos_7',
+    'rhel_7',
+]
+
+
+WINDOWS_OSES = [
+    'windows_2012',
+]
+
+
+def _prepare(cli_host, example, paths, logger, include_secret=True):
     use_ssl = False
     if example.manager.api_ca_path:
         cli_host.put_remote_file(paths['cert'], example.manager.api_ca_path)
@@ -23,13 +35,14 @@ def _prepare(cli_host, example, paths, logger):
         powershell=True,
     )
 
-    logger.info('Creating secret')
-    cli_host.run_command(
-        '{cfy} secrets create --secret-file {ssh_key} agent_key'.format(
-            **paths
-        ),
-        powershell=True,
-    )
+    if include_secret:
+        logger.info('Creating secret')
+        cli_host.run_command(
+            '{cfy} secrets create --secret-file {ssh_key} agent_key'.format(
+                **paths
+            ),
+            powershell=True,
+        )
 
 
 def _test_upload_and_install(run, example, paths, logger):
@@ -186,32 +199,159 @@ def _test_teardown(run, example, paths, logger):
     assert len(blueprints) == 0
 
 
-def get_linux_image_settings():
-    return [
-        ('centos_7', 'rhel_centos_cli_package_url', 'rpm'),
-        ('rhel_7', 'rhel_centos_cli_package_url', 'rpm'),
-    ]
+def _cleanup_profile(run, example, paths, logger, include_secret=True):
+    if include_secret:
+        logger.info('Creating secret')
+        run('{cfy} secrets delete agent_key'.format(
+                **paths),
+            powershell=True)
+
+    logger.info('Removing CLI profile')
+    run('{cfy} profiles delete {ip}'.format(
+        cfy=paths['cfy'],
+        ip=example.manager.private_ip_address),
+        powershell=True)
 
 
-def _install_linux_cli(cli_host, logger, url_key, pkg_type, test_config):
+def _install_linux_cli(cli_host, logger, test_config):
     logger.info('Downloading CLI package')
-    cli_package_url = get_cli_package_url(url_key, test_config)
+    cli_package_url = get_cli_package_url('rhel_centos_cli_package_url',
+                                          test_config)
     logger.info('Using CLI package: {url}'.format(
         url=cli_package_url,
     ))
-    cli_host.run_command('curl -Lo cloudify-cli.{pkg_type} {url}'.format(
-        url=cli_package_url, pkg_type=pkg_type,
+    cli_host.run_command('curl -Lo cloudify-cli.rpm {url}'.format(
+        url=cli_package_url,
     ))
 
     logger.info('Installing CLI package')
-    install_cmd = {
-        'rpm': 'yum install -y',
-        'deb': 'dpkg -i',
-    }[pkg_type]
+    install_cmd = 'yum install -y'
     cli_host.run_command(
-        '{install_cmd} cloudify-cli.{pkg_type}'.format(
+        '{install_cmd} cloudify-cli.rpm'.format(
             install_cmd=install_cmd,
-            pkg_type=pkg_type,
         ),
         use_sudo=True,
     )
+
+
+def _prepare_linux_cli_test_components(cli_host, manager_host, cli_os,
+                                       ssh_key, logger, test_config):
+    _install_linux_cli(cli_host, logger, test_config)
+
+    example = get_example_deployment(
+        manager_host, ssh_key, logger, 'cli_test_{}'.format(cli_os),
+        test_config)
+    example.inputs['path'] = '/tmp/{}'.format(cli_os)
+
+    logger.info('Copying blueprint to CLI host')
+    cli_host.run_command('mkdir -p /tmp/test_blueprint')
+    remote_blueprint_path = '/tmp/test_blueprint/blueprint.yaml'
+    cli_host.put_remote_file(
+        remote_path=remote_blueprint_path,
+        local_path=example.blueprint_file,
+    )
+
+    logger.info('Copying inputs to CLI host')
+    remote_inputs_path = '/tmp/test_blueprint/inputs.yaml'
+    cli_host.put_remote_file_content(
+        remote_path=remote_inputs_path,
+        content=json.dumps(example.inputs),
+    )
+
+    logger.info('Copying agent ssh key to CLI host for secret')
+    remote_ssh_key_path = '/tmp/cli_test_ssh_key.pem'
+    cli_host.put_remote_file(
+        remote_path=remote_ssh_key_path,
+        local_path=ssh_key.private_key_path,
+    )
+
+    return {
+        'cli_host': cli_host,
+        'example': example,
+        'windows': False,
+        'paths': {
+            'blueprint': remote_blueprint_path,
+            'inputs': remote_inputs_path,
+            'ssh_key': remote_ssh_key_path,
+            # Expected to be in path on linux systems
+            'cfy': 'cfy',
+            'cert': '/home/{user}/manager.crt'.format(
+                user=cli_host.username),
+        },
+    }
+
+
+def _prepare_windows_cli_test_components(cli_host, manager_host, cli_os,
+                                         ssh_key, logger, test_config):
+    url_key = 'windows_cli_package_url'
+
+    cli_host.wait_for_winrm()
+
+    work_dir = 'C:\\Users\\{0}'.format(cli_host.username)
+    cli_installer_exe_name = 'cloudify-cli.exe'
+    cli_installer_exe_path = '{0}\\{1}'.format(work_dir,
+                                               cli_installer_exe_name)
+
+    logger.info('Downloading CLI package')
+    cli_package_url = get_cli_package_url(url_key, test_config)
+    cli_package_version = cli_package_url.split('.exe')[-2].split('_')[-1]
+    logger.info('Using CLI package: {url}'.format(
+        url=cli_package_url,
+    ))
+    cli_host.run_command(
+        """
+$client = New-Object System.Net.WebClient
+$url = "{0}"
+$file = "{1}"
+$client.DownloadFile($url, $file)""".format(
+            cli_package_url,
+            cli_installer_exe_path
+        ),
+        powershell=True,
+    )
+
+    logger.info('Installing CLI...')
+    cli_host.run_command(
+        '''
+cd {0}
+& .\\{1} /SILENT /VERYSILENT /SUPPRESSMSGBOXES'''
+        .format(work_dir, cli_installer_exe_name),
+        powershell=True,
+    )
+
+    example = get_example_deployment(
+        manager_host, ssh_key, logger, url_key, test_config,
+        vm=cli_host)
+    example.use_windows(cli_host.username, cli_host.password)
+
+    logger.info('Copying blueprint to CLI host')
+    remote_blueprint_path = work_dir + '\\Documents\\blueprint.yaml'
+    with open(example.blueprint_file) as blueprint_handle:
+        blueprint = blueprint_handle.read()
+    cli_host.put_remote_file_content(remote_blueprint_path, blueprint)
+
+    logger.info('Copying inputs to CLI host')
+    remote_inputs_path = work_dir + '\\Documents\\inputs.yaml'
+    cli_host.put_remote_file_content(remote_inputs_path,
+                                     json.dumps(example.inputs))
+
+    logger.info('Copying agent ssh key to CLI host for secret')
+    remote_ssh_key_path = work_dir + '\\Documents\\ssh_key.pem'
+    with open(ssh_key.private_key_path) as ssh_key_handle:
+        ssh_key_data = ssh_key_handle.read()
+    cli_host.put_remote_file_content(remote_ssh_key_path, ssh_key_data)
+
+    return {
+        'cli_host': cli_host,
+        'example': example,
+        'windows': True,
+        'paths': {
+            'blueprint': remote_blueprint_path,
+            'inputs': remote_inputs_path,
+            'ssh_key': remote_ssh_key_path,
+            'cfy': '&"C:\\Program Files\\Cloudify {} CLI\\Scripts\\'
+                   'cfy.exe"'.format(cli_package_version),
+            'cert': '"C:\\Users\\{username}\\manager.crt"'.format(
+                username=cli_host.username),
+        },
+    }
---
+++ cosmo_tester/test_suites/cli/aio_test.py
@@ -0,0 +1,58 @@
+from cosmo_tester.test_suites.cli import (
+    _cleanup_profile,
+    LINUX_OSES,
+    _prepare,
+    _test_cfy_install,
+    _test_cfy_logs,
+    _test_teardown,
+    _test_upload_and_install,
+    WINDOWS_OSES,
+)
+
+
+def test_cli_deployment_flow(cli_tester, logger):
+    for os in LINUX_OSES + WINDOWS_OSES:
+        cli_host = cli_tester[os]['cli_host']
+        example = cli_tester[os]['example']
+        paths = cli_tester[os]['paths']
+
+        _prepare(cli_host, example, paths, logger)
+
+        _test_upload_and_install(cli_host.run_command, example, paths, logger)
+
+        _test_teardown(cli_host.run_command, example, paths, logger)
+
+        _cleanup_profile(cli_host.run_command, example, paths, logger)
+
+
+def test_cli_install_flow(cli_tester, logger):
+    for os in LINUX_OSES + WINDOWS_OSES:
+        cli_host = cli_tester[os]['cli_host']
+        example = cli_tester[os]['example']
+        paths = cli_tester[os]['paths']
+
+        _prepare(cli_host, example, paths, logger)
+
+        _test_cfy_install(cli_host.run_command, example, paths, logger)
+
+        _test_teardown(cli_host.run_command, example, paths, logger)
+
+        _cleanup_profile(cli_host.run_command, example, paths, logger)
+
+
+def test_cli_logs(cli_tester, logger):
+    # We can't currently cleanly test logs on windows (need CLI unzip on 2012,
+    # but it's only in powershell from 2016(?)).
+    for os in LINUX_OSES:
+        cli_host = cli_tester[os]['cli_host']
+        example = cli_tester[os]['example']
+        paths = cli_tester[os]['paths']
+        tmpdir = cli_tester['tmpdir']
+
+        _prepare(cli_host, example, paths, logger, include_secret=False)
+
+        _test_cfy_logs(cli_host.run_command, cli_host, os, example, paths,
+                       tmpdir, logger)
+
+        _cleanup_profile(cli_host.run_command, example, paths, logger,
+                         include_secret=False)
