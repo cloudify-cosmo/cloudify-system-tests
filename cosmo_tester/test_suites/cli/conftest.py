75fb94a1aa933115b25b1e624429efc5fb10e936
Madda <nobody@nowhere.local>
2021-06-18 16:52:36 +0100

    RD-2623 Make cluster CLI test use fewer VMs

--- cosmo_tester/test_suites/cli/conftest.py
+++ cosmo_tester/test_suites/cli/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
 from cosmo_tester.framework.test_hosts import Hosts, VM
+from cosmo_tester.test_suites.cluster.conftest import _get_hosts
 
 from . import (
     LINUX_OSES,
@@ -29,11 +30,12 @@ def cli_tester(request, ssh_key, session_tmpdir, test_config,
 
     passed = True
 
-    cli_tests_dict = {'manager': manager,
+    cli_tests_dict = {'managers': [manager],
                       'tmpdir': session_tmpdir}
 
     try:
         hosts.create()
+        manager.wait_for_ssh()
 
         for cli_os in all_targets:
             cli_host = cli_vms[cli_os]
@@ -46,6 +48,58 @@ def cli_tester(request, ssh_key, session_tmpdir, test_config,
 
             cli_tests_dict[cli_os] = prep_func(
                 cli_host, manager, cli_os, ssh_key, session_logger,
+                test_config)
+
+        yield cli_tests_dict
+    except Exception:
+        passed = False
+        raise
+    # Do not put this in a finally, let pytest handle that
+    # Otherwise, --pdb will run /after/ the teardown
+    hosts.destroy(passed=passed)
+
+
+@pytest.fixture(scope='session')
+def cluster_cli_tester(request, ssh_key, session_tmpdir, test_config,
+                       session_logger):
+    # Not including windows as we currently don't have any cluster tests that
+    # target windows (see aio logs test for reason)
+    all_targets = LINUX_OSES
+
+    hosts = Hosts(
+        ssh_key, session_tmpdir,
+        test_config, session_logger, request, len(all_targets) + 3,
+        bootstrappable=True,
+    )
+
+    cli_vms = {}
+    for idx, cli_os in enumerate(all_targets):
+        hosts.instances[idx + 3] = VM(cli_os, test_config)
+        cli_vms[cli_os] = hosts.instances[idx + 3]
+
+    passed = True
+
+    try:
+        hosts.create()
+
+        mgr1, mgr2, mgr3 = _get_hosts(
+            hosts.instances[:3], test_config, session_logger,
+            pre_cluster_rabbit=True, three_nodes_cluster=True)
+
+        cli_tests_dict = {'managers': [mgr1, mgr2, mgr3],
+                          'tmpdir': session_tmpdir}
+
+        for cli_os in all_targets:
+            cli_host = cli_vms[cli_os]
+            windows = 'windows' in cli_os
+
+            if windows:
+                prep_func = _prepare_windows_cli_test_components
+            else:
+                prep_func = _prepare_linux_cli_test_components
+
+            cli_tests_dict[cli_os] = prep_func(
+                cli_host, mgr1, cli_os, ssh_key, session_logger,
                 test_config)
 
         yield cli_tests_dict
