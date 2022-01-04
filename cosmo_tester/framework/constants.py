f1063e70fa715d231aae5268cee202704502b205
Madda <nobody@nowhere.local>
2022-01-04 14:59:10 +0000

    Update tested versions

--- cosmo_tester/framework/constants.py
+++ cosmo_tester/framework/constants.py
@@ -3,14 +3,13 @@ CLOUDIFY_TENANT_HEADER = 'Tenant'
 SUPPORTED_RELEASES = [
     '5.0.5',
     '5.1.0',
-    '5.1.1',
-    '5.1.2',
-    '5.1.3',
+    '5.1.1',  # Keeping this version because here we started better versioning
     '5.1.4',
     '5.2.0',
-    '5.2.1',
-    '5.2.2',
+    '5.2.7',
     '6.0.0',
+    '6.1.0',
+    '6.2.0',
     'master',
 ]
 
