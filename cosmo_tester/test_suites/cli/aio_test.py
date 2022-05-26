9a721bacfd85c71d079df1fd0a92ff44685b83f7
geokala <geokala@users.noreply.github.com>
2022-05-26 09:54:00 +0100

    RD-3813 Test log-bundles instead of cfy logs (#1438)
    * RD-3813 Test log-bundles instead of cfy logs
    
    * Flake

--- cosmo_tester/test_suites/cli/aio_test.py
+++ cosmo_tester/test_suites/cli/aio_test.py
@@ -3,8 +3,6 @@ from cosmo_tester.test_suites.cli import (
     LINUX_OSES,
     _prepare,
     _test_cfy_install,
-    _test_cfy_logs,
-    _test_logs_context,
     _test_teardown,
     _test_upload_and_install,
     WINDOWS_OSES,
@@ -39,23 +37,3 @@ def test_cli_install_flow(cli_tester, logger):
         _test_teardown(cli_host.run_command, example, paths, logger)
 
         _cleanup_profile(cli_host.run_command, example, paths, logger)
-
-
-def test_cli_logs(cli_tester, logger):
-    # We can't currently cleanly test logs on windows (need CLI unzip on 2012,
-    # but it's only in powershell from 2016(?)).
-    for os in LINUX_OSES:
-        cli_host = cli_tester[os]['cli_host']
-        example = cli_tester[os]['example']
-        paths = cli_tester[os]['paths']
-        tmpdir = cli_tester['tmpdir']
-
-        _prepare(cli_host, example, paths, logger, include_secret=False)
-
-        with _test_logs_context(cli_host.run_command, example, paths,
-                                cli_tester['managers']):
-            _test_cfy_logs(cli_host.run_command, cli_host, os, example, paths,
-                           tmpdir, logger)
-
-        _cleanup_profile(cli_host.run_command, example, paths, logger,
-                         include_secret=False)
