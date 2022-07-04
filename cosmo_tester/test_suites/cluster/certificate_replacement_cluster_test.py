5f6bec7e3ccff1f3a92d10916804754fe08db243
glukhman <47591609+glukhman@users.noreply.github.com>
2022-07-04 14:43:27 +0300

    RD-2705 Add certs test for 3-node cluster (#1443)
    * RD-2705 Add certs test for 3-node cluster
    * RD-2705 Add tests for replacing CA key

--- cosmo_tester/test_suites/cluster/certificate_replacement_cluster_test.py
+++ cosmo_tester/test_suites/cluster/certificate_replacement_cluster_test.py
@@ -9,7 +9,7 @@ from cosmo_tester.framework.util import (get_resource_path,
 
 @pytest.mark.nine_vms
 def test_replace_certificates_on_cluster(full_cluster_ips, logger, ssh_key,
-                                         test_config):
+                                         test_config, replace_ca_key=False):
     broker1, broker2, broker3, db1, db2, db3, mgr1, mgr2, mgr3 = \
         full_cluster_ips
 
@@ -28,7 +28,8 @@ def test_replace_certificates_on_cluster(full_cluster_ips, logger, ssh_key,
         mgr1.run_command('chmod 444 {0}'.format(key_path), use_sudo=True)
     replace_certs_config_path = '~/certificates_replacement_config.yaml'
     _create_replace_certs_config_file(mgr1, replace_certs_config_path,
-                                      ssh_key.private_key_path)
+                                      ssh_key.private_key_path,
+                                      replace_ca_key=replace_ca_key)
 
     mgr1.run_command('cfy certificates replace -i {0} -v'.format(
         replace_certs_config_path))
@@ -38,9 +39,56 @@ def test_replace_certificates_on_cluster(full_cluster_ips, logger, ssh_key,
     example.uninstall()
 
 
+@pytest.mark.three_vms
+def test_replace_certificates_on_compact_cluster(
+        three_nodes_cluster, logger, ssh_key, test_config,
+        replace_ca_key=False):
+    node1, node2, node3 = three_nodes_cluster
+
+    example = get_example_deployment(node1, ssh_key, logger,
+                                     'cluster_replace_certs', test_config)
+    example.inputs['server_ip'] = node1.ip_address
+    example.upload_and_verify_install()
+    validate_cluster_status_and_agents(node1, example.tenant, logger)
+
+    for host in node1, node2, node3:
+        key_path = join('~', '.cloudify-test-ca',
+                        host.private_ip_address + '.key')
+        node1.run_command('cfy_manager generate-test-cert'
+                          ' -s {0},{1}'.format(host.private_ip_address,
+                                               host.ip_address))
+        node1.run_command('chmod 444 {0}'.format(key_path), use_sudo=True)
+    replace_certs_config_path = '~/certificates_replacement_config.yaml'
+    _create_replace_certs_config_file(node1, replace_certs_config_path,
+                                      ssh_key.private_key_path,
+                                      replace_ca_key=replace_ca_key)
+
+    node1.run_command('cfy certificates replace -i {0} -v'.format(
+        replace_certs_config_path))
+    node1.download_rest_ca(force=True)
+
+    validate_cluster_status_and_agents(node1, example.tenant, logger)
+    example.uninstall()
+
+
+@pytest.mark.nine_vms
+def test_replace_certificates_on_cluster_incl_ca_key(
+        full_cluster_ips, logger, ssh_key, test_config):
+    test_replace_certificates_on_cluster(full_cluster_ips, logger, ssh_key,
+                                         test_config, replace_ca_key=True)
+
+
+@pytest.mark.three_vms
+def test_replace_certificates_compact_cluster_incl_ca_key(
+        three_nodes_cluster, logger, ssh_key, test_config):
+    test_replace_certificates_on_compact_cluster(
+        three_nodes_cluster, logger, ssh_key, test_config, replace_ca_key=True)
+
+
 def _create_replace_certs_config_file(manager,
                                       replace_certs_config_path,
-                                      local_ssh_key_path):
+                                      local_ssh_key_path,
+                                      replace_ca_key=False):
     remote_script_path = join('/tmp', 'create_replace_certs_config_script.py')
     remote_ssh_key_path = '~/.ssh/ssh_key.pem'
 
@@ -51,6 +99,7 @@ def _create_replace_certs_config_file(manager,
     local_script_path = get_resource_path(
         'scripts/create_replace_certs_config_script.py')
     manager.put_remote_file(remote_script_path, local_script_path)
-    command = '/opt/cfy/bin/python {0} --output {1} --cluster'.format(
-        remote_script_path, replace_certs_config_path)
+    command = '/opt/cfy/bin/python {0} --output {1} --replace-ca-key {2} ' \
+              '--cluster'.format(
+                remote_script_path, replace_certs_config_path, replace_ca_key)
     manager.run_command(command)
