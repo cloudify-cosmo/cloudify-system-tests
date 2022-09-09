aa036bc6a0f97428f5fa7719f37c3504c7b434ae
Mateusz Neumann <mateumann@gmail.com>
2022-09-09 11:33:22 +0200

    Set the tenant when executing operations

--- cosmo_tester/test_suites/agent/test_retry.py
+++ cosmo_tester/test_suites/agent/test_retry.py
@@ -2,6 +2,7 @@ import time
 
 from cosmo_tester.framework.examples import get_example_deployment
 from cosmo_tester.framework.test_hosts import Hosts, VM
+from cosmo_tester.framework.util import set_client_tenant
 
 AGENT_OSES = [
     'centos_8',
@@ -29,7 +30,7 @@ def test_agent_retry(ssh_key, module_tmpdir, test_config, logger, request):
         # we still have them if we need to troubleshoot cross-contamination.
         examples = {
             agent_os: get_example_deployment(
-                manager, ssh_key, logger, 'agent_retry_{}'.format(agent_os),
+                manager, ssh_key, logger, f'agent_retry_{agent_os}',
                 test_config, vm=agent_vms[agent_os]
             )
             for agent_os in AGENT_OSES
@@ -38,6 +39,7 @@ def test_agent_retry(ssh_key, module_tmpdir, test_config, logger, request):
         for agent_os in AGENT_OSES:
             example = examples[agent_os]
             vm = agent_vms[agent_os]
+            tenant_name = f'agent_retry_{agent_os}'
 
             example.inputs['wait'] = 20
             example.upload_blueprint()
@@ -45,8 +47,9 @@ def test_agent_retry(ssh_key, module_tmpdir, test_config, logger, request):
             if 'windows' in agent_os:
                 example.use_windows(vm.username, vm.password)
 
-            execution_id = manager.client.executions.start(
-                example.deployment_id, 'install').id
+            with set_client_tenant(manager.client, tenant_name):
+                execution_id = manager.client.executions.start(
+                    example.deployment_id, 'install').id
             time.sleep(3)  # wait for the mgmtworker to get the execution
 
             # Disable communication with agent for a few moments
@@ -64,13 +67,15 @@ def test_agent_retry(ssh_key, module_tmpdir, test_config, logger, request):
             timeout, delay = 120, 15
             while timeout > 0:
                 try:
-                    manager.client.executions.get(execution_id)
+                    with set_client_tenant(manager.client, tenant_name):
+                        manager.client.executions.get(execution_id)
                 except Exception:
                     time.sleep(delay)
                     timeout -= delay
 
             # Validate the execution
-            execution = manager.client.executions.get(execution_id)
+            with set_client_tenant(manager.client, tenant_name):
+                execution = manager.client.executions.get(execution_id)
             assert execution.status == 'terminated'
 
             example.uninstall()
