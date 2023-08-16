dfe1b30870a956e7122438442a907b2c9bd8e604
glukhman <47591609+glukhman@users.noreply.github.com>
2023-08-16 19:17:44 +0300

    Use FQDNS in cluster tests (#1570)
    Co-authored-by: glukhman <michael.glukhman@dell.com>

--- cosmo_tester/test_suites/cluster/compact_cfy_cluster_manager_upgrade_test.py
+++ cosmo_tester/test_suites/cluster/compact_cfy_cluster_manager_upgrade_test.py
@@ -7,8 +7,8 @@ from cosmo_tester.framework.constants import SUPPORTED_FOR_RPM_UPGRADE
 @pytest.mark.three_vms
 @pytest.mark.upgrade
 @pytest.mark.parametrize('base_version', SUPPORTED_FOR_RPM_UPGRADE)
-def test_three_nodes_cluster_upgrade(base_version, three_vms_fqdns,
-                                     test_config, ssh_key, logger):
+def test_three_nodes_cluster_upgrade(base_version, three_vms, test_config,
+                                     ssh_key, logger):
     """Tests the command cfy_cluster_manager upgrade on a 3 nodes cluster."""
-    _cluster_upgrade_test(test_config, base_version, three_vms_fqdns, ssh_key,
+    _cluster_upgrade_test(test_config, base_version, three_vms, ssh_key,
                           logger)
